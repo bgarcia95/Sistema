415cb683fb6cbb6a427ee429f41c4700d540c18b
bgarcia95 <bgarcia.95@outlook.com>
2019-07-07 23:34:50 -0600

    Add project files.

---
+++ Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Entidades.Almacen
+{
+    class Categoria
+    {
+        public int id_categoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe tener mas de 50 caracteres, ni menos de 3 caracteres")]
+        public string nombre { get; set; }
+        [StringLength(256)]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
